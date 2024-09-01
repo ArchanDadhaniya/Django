e2e72c136231b90214b0205b15a4629663abefc9
Archan Dadhaniya <archandadhaniya@yahoo.com>
2024-09-01 20:58:30 +0530

    fetch object from the database.

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -1,10 +1,19 @@
+
 from django.http import HttpResponse
-from django.shortcuts import render
+
+from food.models import Item
+from django.template import loader
+
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    item_list = Item.objects.all()
+    template = loader.get_template('food/index.html')
+    context = {
+        'item_list': item_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def item(request):
     return HttpResponse("Hello, world. This is an item view.")
