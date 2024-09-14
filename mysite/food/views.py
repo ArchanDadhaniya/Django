0d1220b631ae8938297fe757fe551bee2b61dbc0
Archan Dadhaniya <archandadhaniya@yahoo.com>
2024-09-15 00:54:49 +0530

    make the url with the food name and provide the detail of that food.

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -1,5 +1,6 @@
 
 from django.http import HttpResponse
+from django.shortcuts import render
 
 from .models import Item
 from django.template import loader
@@ -9,11 +10,18 @@ from django.template import loader
 
 def index(request):
     item_list = Item.objects.all()
-    template = loader.get_template('food/index.html')
     context = {
         'item_list': item_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'food/index.html', context)
+
+
+def detail(request, item_id):
+    item = Item.objects.get(pk=item_id)
+    context = {
+        'item': item,
+    }
+    return render(request, 'food/detail.html', context)
 
 def item(request):
     return HttpResponse("Hello, world. This is an item view.")
